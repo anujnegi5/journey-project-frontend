7f5bb37ccb04d892b468e408c36721faf1879d93
Anuj Negi <anuj5negi5@gmail.com>
2022-12-22 20:48:28 +0530

    changed the API address

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://journey-project-backend.vercel.app/",
+  baseURL: "https://journeyprojectbackend-production.up.railway.app/",
 });
 
 API.interceptors.request.use((req) => {
