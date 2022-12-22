a43ca9412c1c98607ec172b48207fe9835224ee6
Anuj Negi <anuj5negi5@gmail.com>
2022-12-22 21:12:00 +0530

    changed the server to render.com

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://journeyprojectbackend-production.up.railway.app/",
+  baseURL: "https://journey-project-backend.onrender.com/",
 });
 
 API.interceptors.request.use((req) => {
